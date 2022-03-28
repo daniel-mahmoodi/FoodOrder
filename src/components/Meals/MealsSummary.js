d824ad3a3802e388b528ecda471c7935d7f10cf3
unknown <mahmoodidanial2@gmail.com>
2022-03-28 18:26:09 +0430

    2nd edit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from './MealsSummary.module.css'
+const MealsSummary =()=>{
+     return <section className={classes.summary}>
+          <h2>
+               Delicious Food, Delivered To You
+          </h2>
+               <p>
+                    Choose your favoraite meal from our broad selection of variable meals 
+                    and enjoy a delecious lunch dinner at home.
+               </p>
+               <p>
+                    All your meals are cooked with high-quality ingrediants, just-in-time and 
+                    of course by exprienced chef!
+               </p>
+     </section>
+};
+
+export default MealsSummary;

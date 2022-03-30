eba154569ccd2b35a6f247750caa00ad3f6cb940
unknown <mahmoodidanial2@gmail.com>
2022-03-30 17:50:16 +0430

    add Components

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm'
 import classes from './MealItem.module.css';
 
 const MealItem = props =>{
@@ -5,11 +6,11 @@ const MealItem = props =>{
      return <li className={classes.meal}>
           <div><h3>{props.name}</h3>
           <div className={classes.description}>{props.description}</div>
-          <div className={classes.price} >{price}</div>
+          <div className={classes.price}>{price}</div>
           </div>
           <div>
-               
+               <MealItemForm/> 
           </div>
-     </li>
+     </li> 
 }
 export default MealItem;
